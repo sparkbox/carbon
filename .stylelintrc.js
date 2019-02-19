2730c92264d3063715b6ea92dd851504da0a4f2b
Adam Simpson <adam@heysparkbox.com>
2019-02-19 11:00:22 -0500

    feat: rely soley on sparkbox-stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,26 +1,3 @@
 module.exports = {
   "extends": "stylelint-config-sparkbox",
-  "plugins": [
-    "stylelint-scss"
-  ],
-  "env": {
-    "node": true,
-    "browser": true
-  },
-  "rules": {
-    "max-empty-lines": [2, { ignore: ["comments"] }],
-    "max-nesting-depth": 4,
-    "number-no-trailing-zeros": true,
-    "number-leading-zero": "always",
-    "no-empty-source": null,
-    "color-hex-case": "upper",
-    "color-hex-length": "long",
-    "block-closing-brace-newline-after": null,
-    "declaration-empty-line-before": null,
-    "declaration-colon-newline-after": "always-multi-line",
-    "selector-list-comma-newline-after": "always-multi-line",
-    "at-rule-no-unknown": null,
-    "function-linear-gradient-no-nonstandard-direction": null,
-    "no-eol-whitespace": null
-  }
 }
