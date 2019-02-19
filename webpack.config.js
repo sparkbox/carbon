ce8b5af56cbee7185eeb68007da1e87fd53c05da
Adam Simpson <adam@heysparkbox.com>
2019-02-19 11:11:26 -0500

    feat: update webpack/eslint

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
     ],
