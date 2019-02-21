c927c6c171c6167422e665a0c045a39af2448e64
Patrick Fulton <fulton@heysparkbox.com>
2019-02-21 10:45:31 -0500

    feat: webpack-dev-server
    This adds `webpack-dev-server` to the project, and sets some configs for it (and JS in the project).
    
    This also updates several dependencies, which needed to be done in order for `webpack-dev-server` to work.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 module.exports = {
   devtool: process.env.NODE_ENV === 'production' ? 'none' : 'inline-source-map',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, './dist/js'),
+  },
   module: {
     rules: [
       {
