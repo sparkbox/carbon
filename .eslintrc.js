1efb20db4ecc9fd47b58b889d9033b2e57643213
iAmNathanJ <jacobs@heysparkbox.com>
2019-07-12 09:08:12 -0400

    eslint: add notes about env option

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
   extends: 'sparkbox',
+  overrides: [
+    {
+      files: ['**/*test.js', '**/*spec.js'],
+      // env options will allow all of the global variable names
+      // for specific authoring environments, such as testing frameworks
+      // add your testing environment here
+      env: {
+        mocha: true
+        // jasmine: true,
+        // jest: true,
+      },
+      rules: {
+        'no-unused-expressions': 'off'
+      }
+    }
+  ]
 };
--- specs/default-spec.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('Placeholder test', () => {
-  it('should pass', () => {
-    expect(true).to.be.true;
-  });
-});
