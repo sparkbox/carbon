73267a6b9afa985ce7d1edd444431f6d77991c58
iAmNathanJ <jacobs@heysparkbox.com>
2019-07-12 09:14:47 -0400

    babel: convert .babelrc to babel.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ module.exports = {
   module: {
     rules: [
       {
+      {
+        // JS
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+          loader: 'babel-loader'
+        }
           },
         },
       },
